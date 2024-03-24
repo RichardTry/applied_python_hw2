91a7f555c8e984fe4fd179404d7a759b60ce550c
RichardTry <bazrovstepan@gmail.com>
2024-03-24 16:58:02 +0300

    test 1

--- tests/test_1.py
+++ tests/test_1.py
@@ -9,7 +9,7 @@ from aiogram_tests.types.dataset import MESSAGE
 
 @pytest.mark.asyncio
 async def test_echo():
-    request = MockedBot(MessageHandler(cmd_rate))
-    calls = await request.query()
-    answer_message = calls.send_messsage.fetchone()
-    assert answer_message.text == "Hello, Bot!"
+   request = MockedBot(MessageHandler(cmd_rate))
+   calls = await request.query()
+   answer_message = calls.send_messsage.fetchone()
+   assert answer_message.text == "Hello, Bot!"
